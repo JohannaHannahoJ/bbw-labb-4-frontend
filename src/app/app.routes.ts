f3c81aff6ae32ff92caddc7d57ad138ed6a9ba5e
JohannaHannahoJ <joha2102@student.miun.se>
2026-05-06 19:09:15 +0200

    Add routing and 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import { Dashboard } from './dashboard/dashboard';
+import { CreateEntry } from './create-entry/create-entry';
+import { NotFound } from './not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: 'dashboard', component: Dashboard },
+    { path: 'create-entry', component: CreateEntry },
+    { path: "404", component: NotFound },
+    { path: "**", redirectTo: "404", pathMatch: "full" }
+];
