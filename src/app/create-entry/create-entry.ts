1741cb13b059b77fa1097547749846c635767d86
JohannaHannahoJ <joha2102@student.miun.se>
2026-05-06 16:39:59 +0200

    Create Angular project with basic structure and component

---
+++ src/app/create-entry/create-entry.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-entry',
+  imports: [],
+  templateUrl: './create-entry.html',
+  styleUrl: './create-entry.css',
+})
+export class CreateEntry {}
