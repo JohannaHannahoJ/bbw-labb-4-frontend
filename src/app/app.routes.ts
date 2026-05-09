77a71239d39f880952005030c440c9a00be6f91c
JohannaHannahoJ <joha2102@student.miun.se>
2026-05-09 22:18:21 +0200

    Add auth guard for protected routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,14 @@ import { Register } from './register/register';
 import { Dashboard } from './dashboard/dashboard';
 import { CreateEntry } from './create-entry/create-entry';
 import { NotFound } from './not-found/not-found';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: Login },
     { path: 'register', component: Register },
-    { path: 'dashboard', component: Dashboard },
-    { path: 'create-entry', component: CreateEntry },
+    { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+    { path: 'create-entry', component: CreateEntry, canActivate: [authGuard] },
     { path: "404", component: NotFound },
     { path: "**", redirectTo: "404", pathMatch: "full" }
 ];
